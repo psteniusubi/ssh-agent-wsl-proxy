86001066882f7c759ab35cf2b85238c8cae6f636
Petteri Stenius <petteri.stenius@gmail.com>
2024-10-02 22:34:23 +0300

    add build script

---
+++ build/set-release.ps1
@@ -0,0 +1,24 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [version]
+    $Version
+)
+process {
+    $root = Split-Path $PSScriptRoot -Parent -Resolve -ErrorAction Stop
+    $csproj = Join-Path $root "ssh-agent-wsl-proxy/ssh-agent-wsl-proxy.csproj" -Resolve -ErrorAction Stop | Convert-Path
+    
+    $xml = [xml]::new()
+    $xml.PreserveWhitespace = $true
+    $null = $xml.Load($csproj)
+    $ProjectVersion = $xml.SelectSingleNode("/Project/PropertyGroup/Version")
+    $ProjectVersion.InnerText = $Version
+    $null = $xml.Save($csproj)
+
+    Write-Host -ForegroundColor Green @"
+git commit -a -m "release v$($Version)"
+git push
+git branch "release/v$($Version)"
+git push origin "release/v$($Version)"
+"@
+}
